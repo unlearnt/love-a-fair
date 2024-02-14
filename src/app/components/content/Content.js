b7b96b0a28c0a7fe8b69926566edf706597707fb
unlearnt <unlearnt92@gmail.com>
2024-02-14 16:25:55 +0800

    update

--- src/app/components/content/Content.js
+++ src/app/components/content/Content.js
@@ -6,7 +6,6 @@ import sampleImageTwo from '@/images/sample_image2.png'
 import oogaImage from '@/images/oogachaga.png'
 import {ChevronRightIcon} from "@heroicons/react/24/solid";
 
-
 export default function Content() {
     return (
         <div className="bg-white px-4 py-8 lg:px-8 w-full">
@@ -56,7 +55,7 @@ export default function Content() {
                                     quality={100}
                                     alt="Hero Image"
                                     className="absolute inset-0 w-full h-full object-cover rounded-xl"
-                                    style={{objectFit:"cover"}}
+                                    style={{objectFit: "cover"}}
                                 />
                             </div>
                         </div>
@@ -68,7 +67,7 @@ export default function Content() {
                                     quality={100}
                                     alt="Hero Image"
                                     className="absolute inset-0 w-full h-full object-cover rounded-xl"
-                                    style={{objectFit:"cover"}}
+                                    style={{objectFit: "cover"}}
                                 />
                             </div>
                         </div>
@@ -78,96 +77,111 @@ export default function Content() {
                     <h1 className="mt-8 text-3xl font-bold tracking-tight text-[#C92420] sm:text-4xl">Our vendors</h1>
 
                     <p className="mt-6 ">
-                        Here’s a sneak peek at our current lineup of vendors, with more exciting additions on the horizon!
+                        Here’s a sneak peek at our current lineup of vendors, with more exciting additions on the
+                        horizon!
                     </p>
                     <div className="mt-6 w-full grid grid-cols-3 gap-3 text-lg md:text-xl lg:text-2xl text-black">
 
-                        <a href="https://www.onlycreamery.com/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
-                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true" />
+                        <a href="https://www.onlycreamery.com/" target="_blank" rel="noopener noreferrer"
+                           className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
+                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true"/>
                             Only Creamery
                         </a>
 
-                        <a href="https://www.amigosyvinos.com/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
-                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true" />
+                        <a href="https://www.amigosyvinos.com/" target="_blank" rel="noopener noreferrer"
+                           className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
+                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true"/>
                             Amigos Y Vinos
                         </a>
 
-                        <a href="https://heckinunicorn.com/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
-                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true" />
+                        <a href="https://heckinunicorn.com/" target="_blank" rel="noopener noreferrer"
+                           className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
+                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true"/>
                             Heckin&apos; Unicorn
                         </a>
 
-                        <a href="https://www.drips.com.sg/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
-                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true" />
+                        <a href="https://www.drips.com.sg/" target="_blank" rel="noopener noreferrer"
+                           className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
+                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true"/>
                             Drips
                         </a>
 
-                        <a href="https://www.smol.sg/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
-                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true" />
+                        <a href="https://www.smol.sg/" target="_blank" rel="noopener noreferrer"
+                           className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
+                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true"/>
                             Smol
                         </a>
 
-                        <div className="text-xs sm:text-xs md:text-lg lg:text-xl flex items-center gap-2 font-bold text-gray-800">
-                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true" />
+                        <div
+                            className="text-xs sm:text-xs md:text-lg lg:text-xl flex items-center gap-2 font-bold text-gray-800">
+                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true"/>
                             Luna Chang’s Cartoon Portraits
                         </div>
 
 
                         <div className="flex items-center gap-2 font-bold text-gray-800">
-                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true" />
+                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true"/>
                             Miss G
                         </div>
 
-                        <a href="https://www.selfiebox.com.sg/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
-                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true" />
+                        <a href="https://www.selfiebox.com.sg/" target="_blank" rel="noopener noreferrer"
+                           className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
+                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true"/>
                             Selfiebox
                         </a>
 
-                        <a href="https://calypsoul.life/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
-                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true" />
+                        <a href="https://calypsoul.life/" target="_blank" rel="noopener noreferrer"
+                           className="flex items-center gap-2 font-bold text-gray-600 hover:underline">
+                            <ChevronRightIcon className="w-4 h-4 text-black" aria-hidden="true"/>
                             Calypsoul Life
                         </a>
 
                     </div>
 
-                    <h1 className="mt-8 text-3xl font-bold tracking-tight text-[#C92420] sm:text-4xl">Our beneficiary - Oogachaga </h1>
+                    <h1 className="mt-8 text-3xl font-bold tracking-tight text-[#C92420] sm:text-4xl">Our beneficiary -
+                        Oogachaga </h1>
 
                     <p className="mt-6">
                         Join us in celebrating love, diversity, and equality by standing with Oogachaga.
-                        Together, let&apos;s create a world where everyone can thrive, no matter who they are or who they love.
+                        Together, let&apos;s create a world where everyone can thrive, no matter who they are or who
+                        they love.
                     </p>
 
                     <p className="mt-6">
-                        Oogachaga is a Singapore-based non-profit organization dedicated to supporting the LGBTQ+ community.
-                        They offer a range of services including counseling, support groups, educational workshops, and advocacy initiatives.
-                        Oogachaga works to promote acceptance, inclusion, and equality for individuals of all sexual orientations and gender identities in Singapore.
-                        Their mission is to create a safe and supportive environment where LGBTQ+ individuals can access the resources and support they need to live authentically and thrive.
-                        From counseling services to community outreach programs, Oogachaga&apos;s tireless efforts foster inclusivity, acceptance, and empowerment.
-                        Every dollar raised directly contributes to their life-changing work, making a tangible difference in the lives of countless individuals.
+                        Oogachaga is a Singapore-based non-profit organization dedicated to supporting the LGBTQ+
+                        community.
+                        They offer a range of services including counseling, support groups, educational workshops, and
+                        advocacy initiatives.
+                        Oogachaga works to promote acceptance, inclusion, and equality for individuals of all sexual
+                        orientations and gender identities in Singapore.
+                        Their mission is to create a safe and supportive environment where LGBTQ+ individuals can access
+                        the resources and support they need to live authentically and thrive.
+                        From counseling services to community outreach programs, Oogachaga&apos;s tireless efforts
+                        foster inclusivity, acceptance, and empowerment.
+                        Every dollar raised directly contributes to their life-changing work, making a tangible
+                        difference in the lives of countless individuals.
                     </p>
 
 
                     <div className="mt-6 flex flex-wrap sm:flex-nowrap">
                         <span className="mr-2">Find out more about them here:</span>
-                        <a target="_blank" rel="noopener noreferrer" href="https://oogachaga.com/" className="text-blue-500 underline break-words">
+                        <a target="_blank" rel="noopener noreferrer" href="https://oogachaga.com/"
+                           className="text-blue-500 underline break-words">
                             https://oogachaga.com/
                         </a>
                     </div>
 
-                    <figure className="mt-4 flex flex-row gap-6 md:gap-8">
+                    <figure className="mt-4 flex flex-row gap-6 md:gap-8 justify-center">
                         <Image
                             src={oogaImage} // Ensure this is correctly imported or defined
                             // fill
                             quality={100}
                             alt="Hero Image"
-                            className="w-full h-full object-cover rounded-xl mt-6"
+                            className="w-2/5 h-full object-cover rounded-xl mt-6"
                             style={{objectFit: "cover"}}
                         />
                     </figure>
-
                 </div>
-
-
             </div>
         </div>
     )
